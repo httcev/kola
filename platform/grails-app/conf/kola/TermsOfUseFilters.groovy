e07c393066274930acc9a45762f7cef23c726a62
Stephan Tittel <stephan.tittel@httc.de>
2015-10-28 16:17:48 +0100

    fix broken learning resource creation

--- platform/grails-app/conf/kola/TermsOfUseFilters.groovy
+++ platform/grails-app/conf/kola/TermsOfUseFilters.groovy
@@ -6,17 +6,20 @@ class TermsOfUseFilters {
     def springSecurityService
     def grailsApplication
     def messageSource
-    def sessionFactory
 
     def filters = {
         all(controller:'*', action:'*', controllerExclude:'termsOfUse|logout|changes') {
             before = {
                 // only redirect if terms of use are configured and user is logged in and has not accepted already
-                if (grailsApplication.config.kola.termsOfUseExisting && springSecurityService.loggedIn) {
-                    if (sessionFactory.currentSession != null && !springSecurityService.currentUser.termsOfUseAccepted) {
+                if (grailsApplication.config.kola.termsOfUseExisting && springSecurityService.loggedIn && !session.termsOfUseAccepted) {
+                    if (!springSecurityService.currentUser.termsOfUseAccepted) {
                         flash.message = messageSource.getMessage("kola.termsOfUse.accept.please", null , LCH.getLocale())
                         redirect(controller:"termsOfUse", action:"index")
                         return false
+                    }
+                    else {
+                        // need to cache acceptance in session, otherwise we'll get an error in web flows (like creation of learning resources)
+                        session.termsOfUseAccepted = true        
                     }
                 }
             }
