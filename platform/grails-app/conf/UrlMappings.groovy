5c5421a8a3462bd122998e9c41d63ea3b258fe48
Stephan Tittel <stephan.tittel@httc.de>
2016-09-29 08:56:52 +0200

    moved url mappings to plugins

--- platform/grails-app/conf/UrlMappings.groovy
+++ platform/grails-app/conf/UrlMappings.groovy
@@ -13,27 +13,6 @@ class UrlMappings {
 			action = [POST:"update"]
 		}
 
-		"/user/$action?/$id?(.$format)?" { controller = "user"; namespace = "admin"; plugin = "httcUser" }
-		"/taxonomies/$action?/$id?(.$format)?" { controller = "taxonomies"; plugin = "httcTaxonomy" }
-		"/admin/taxonomies/$action?/$id?(.$format)?" { controller = "taxonomies"; namespace = "admin"; plugin = "httcTaxonomy" }
-		"/register/$action?" { controller = "register"; plugin = "httcUser" }
-
-		name rateAnswer: "/question/$questionId/answer/$answerId/rate"{
-			controller = "question"
-			action = "rateAnswer"
-			constraints {
-				// apply constraints here
-			}
-		}
-
-		name acceptAnswer: "/question/$questionId/answer/$answerId/accept"{
-			controller = "question"
-			action = "acceptAnswer"
-			constraints {
-				// apply constraints here
-			}
-		}
-
 		"/$controller/$action?/$id?(.$format)?"{
 			constraints {
 				// apply constraints here
