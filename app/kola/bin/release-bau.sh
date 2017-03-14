67cbef356bb4efbc4b0437a8a47bfd68e31d284e
Stephan Tittel <stephan.tittel@httc.de>
2017-03-14 11:11:13 +0100

    fixes ionic changes since update

--- app/kola/bin/release-bau.sh
+++ app/kola/bin/release-bau.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf plugins/* platforms/*
-ENV=prod gulp
+ENV=bau gulp
 cordova platform add android
 ionic build android --release
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /home/tittel/share/work/httc/httc-keystore/httc.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk httc \
-&& zipalign -f -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk kola-prod.apk
+&& zipalign -f -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk kola-bau.apk
--- app/kola/bin/release-demo.sh
+++ app/kola/bin/release-demo.sh
@@ -2,6 +2,6 @@
 rm -rf plugins/* platforms/*
 ENV=demo gulp
 cordova platform add android
-ionic build --release android
+ionic build android --release
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /home/tittel/share/work/httc/httc-keystore/httc.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk httc \
 && zipalign -f -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk kola-demo.apk
